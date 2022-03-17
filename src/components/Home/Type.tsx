fa0b2d180fa3e510991cdc65d79124657364eec1
Santiago Siordia <santiago.gonzalez.siordia@gmail.com>
2022-03-16 18:10:46 -0600

    trans: typewriter

--- src/components/Home/Type.tsx
+++ src/components/Home/Type.tsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import Typewriter from "typewriter-effect";
 
 function Type() {
+  const { t } = useTranslation();
+
   return (
     <Typewriter
       options={{
         strings: [
-          "Cibersecurity Expert",
-          "Ethical Hacker",
-          "Mobile Developer",
-          "JavaScript Junkie",
-          "React Native Engineer",
-          "Tech lover",
+          t("typewriter.cybersecurity expert"),
+          t("typewriter.ethical hacker"),
+          t("typewriter.mobile developer"),
+          t("typewriter.javascript junkie"),
+          t("typewriter.react native engineer"),
+          t("typewriter.tech lover"),
         ],
         autoStart: true,
         loop: true,
