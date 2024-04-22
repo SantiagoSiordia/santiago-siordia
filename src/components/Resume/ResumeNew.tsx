a8d6bdc3e8c84332836d3e4685dd81e7a6106f10
Santiago <santiago.gonzalez.siordia@gmail.com>
2024-04-22 09:46:12 -0600

    Update translation.json files for multiple languages and add resume page functionality

--- src/components/Resume/ResumeNew.tsx
+++ src/components/Resume/ResumeNew.tsx
@@ -5,6 +5,7 @@ import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import { Particle } from "../Particle";
+import { useTranslation } from "react-i18next";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const pdf = require("../../Assets/santiago-gonzalez-siordia.pdf").default;
@@ -14,6 +15,8 @@ const resumeLink =
 
 export const ResumeNew = () => {
   const [width, setWidth] = useState(1200);
+  const [currentPage, setCurrentPage] = useState(1);
+  const { t } = useTranslation();
 
   useEffect(() => {
     setWidth(window.innerWidth);
@@ -23,23 +26,37 @@ export const ResumeNew = () => {
     <div>
       <Container fluid className="resume-section">
         <Particle />
+        <Row className="resume-header" style={{ justifyContent: "center", position: "relative" }}>
+          <h1>Resume</h1>
+        </Row>
+        <p>{t("resume.currentPage", { pageNumber: currentPage, totalPages: 2 })}</p>
+
+        {/* Previous page button and next page button */}
         <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button variant="primary" href={pdf} target="_blank">
-            <AiOutlineDownload />
-            &nbsp;Download CV
+          <Button
+            variant="primary"
+            onClick={() => {
+              if (currentPage === 1) {
+                setCurrentPage(2);
+              } else {
+                setCurrentPage(1);
+              }
+            }}
+          >
+            {t(currentPage === 1 ? "resume.nextPage" : "resume.previousPage")}
           </Button>
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink}>
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          <Document file={resumeLink} >
+            <Page pageNumber={currentPage} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button variant="primary" href={pdf} target="_blank">
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;{t("resume.download")}
           </Button>
         </Row>
       </Container>
